dba05c8c399b7df66bb515a063297fcbf6c5f29a
star-candy <el0710@naver.com>
2023-11-12 01:27:49 +0900

    feat(christmas): 예약 날짜 저장

--- docs/README.md
+++ docs/README.md
@@ -143,7 +143,7 @@
   
 ### dto package
 - [0] 주문 메뉴 저장 OrderMenu class
-- [ ] 방문 날짜 저장 reservedDate class
+- [0] 방문 날짜 저장 reservedDate class
 - [0] 할인 전 총 주문 금액 prediscountpayment class
 - [0] 증정 메뉴 개수 giftreward class
 - [0] 혜택 내역 저장 orderMenu class
--- src/main/java/DTO/ReservedDate.java
+++ src/main/java/DTO/ReservedDate.java
@@ -1,4 +1,13 @@
 package DTO;
 
 public class ReservedDate {
+    private final int reservedDate;
+
+    public ReservedDate(int reservedDate) {
+        this.reservedDate = reservedDate;
+    }
+
+    public int getReservedDate() {
+        return reservedDate;
+    }
 }
