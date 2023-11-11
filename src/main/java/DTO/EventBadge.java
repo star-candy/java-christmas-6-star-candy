3e00db5f0e51c14a3e852de5002151b544fd59fd
star-candy <el0710@naver.com>
2023-11-12 01:12:58 +0900

    feat(christmas): 이벤트 배지 저장 기능

--- src/main/java/DTO/EventBadge.java
+++ src/main/java/DTO/EventBadge.java
@@ -1,4 +1,13 @@
 package DTO;
 
 public class EventBadge {
+    private final String eventBadge;
+
+    public EventBadge(String eventBadge) {
+        this.eventBadge = eventBadge;
+    }
+
+    public String getEventBadge() {
+        return eventBadge;
+    }
 }
