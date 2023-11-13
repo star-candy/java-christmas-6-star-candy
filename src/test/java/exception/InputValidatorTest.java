b9295360cb64c013731b8ef93e9142237545148f
star-candy <el0710@naver.com>
2023-11-14 03:17:10 +0900

    feat(christmas): 메뉴 최댓값 입력 테스트

--- src/test/java/exception/InputValidatorTest.java
+++ src/test/java/exception/InputValidatorTest.java
@@ -2,17 +2,23 @@ package exception;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 
 class InputValidatorTest {
     @Test
     void 날짜_예외처리_작동_테스트() {
-        //given
         int date = 33;
-        //when
         InputValidator inputValidator = new InputValidator();
         assertThrows(IllegalArgumentException.class, () -> inputValidator.dateValidate(date));
+    }
+
+    @Test
+    void 메뉴_최댓값_입력_테스트() {
+        List<Integer> menuQuantity = List.of(10, 2, 3, 10);
+        InputValidator inputValidator = new InputValidator();
+        assertThrows(IllegalArgumentException.class, () -> inputValidator.menuQuantityValidate(menuQuantity));
     }
 
 }
