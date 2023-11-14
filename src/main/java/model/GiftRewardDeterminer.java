bbe6d7d73679aefe72f085a8ae465d5dee3650d1
star-candy <el0710@naver.com>
2023-11-14 17:31:28 +0900

    refactor(christmas): 구조 수정 및 상수화

--- src/main/java/model/GiftRewardDeterminer.java
+++ src/main/java/model/GiftRewardDeterminer.java
@@ -1,17 +1,19 @@
 package model;
 
 public class GiftRewardDeterminer {
+    private final static int GIFT_OFFER_POINT = 120000;
+    private final static int GIFT_REWARD = 1;
+    private final static int NOT_GIFT_REWARD = 0;
     private final int preDiscountPayment;
 
     public GiftRewardDeterminer(int preDiscountPayment) {
         this.preDiscountPayment = preDiscountPayment;
-
     }
 
     public int giftRewardDetermine() {
-        if(preDiscountPayment >= 120000) {
-            return 1;
+        if (preDiscountPayment >= GIFT_OFFER_POINT) {
+            return GIFT_REWARD;
         }
-        return 0;
+        return NOT_GIFT_REWARD;
     }
 }
