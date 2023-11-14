809e48930fdb7811005114266b7dcce6b62bab51
star-candy <el0710@naver.com>
2023-11-14 15:31:57 +0900

    feat(christmas): 증정이벤트 정상출력 테스트

--- src/test/java/model/DiscountCalculatorTest.java
+++ src/test/java/model/DiscountCalculatorTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class DiscountCalculatorTest {
     @Test
@@ -18,7 +17,7 @@ class DiscountCalculatorTest {
         DiscountCalculator discountCalculator = new DiscountCalculator(date, gift, menuName, menuQuantity);
         int totalDiscount = discountCalculator.getTotalDiscount();
         int discountValidate = 0;
-        List<Integer> discountPayments =  discountCalculator.getDiscountPayments();
+        List<Integer> discountPayments = discountCalculator.getDiscountPayments();
         for (Integer discountPayment : discountPayments) {
             discountValidate += discountPayment;
         }
@@ -38,12 +37,25 @@ class DiscountCalculatorTest {
         int validate = 0;
         List<Integer> validateList = discountCalculator.getDiscountPayments();
         validate = validateList.get(1);
+        //then
         assertThat(validate).isEqualTo(2023);
 
     }
 
     @Test
     void 증정이벤트_정상출력_테스트() {
+        //given
+        int date = 21;
+        int gift = 1;
+        List<String> menuName = List.of("초코케이크", "티본스테이크", "제로콜라");
+        List<Integer> menuQuantity = List.of(3, 3, 3);
+        //when
+        DiscountCalculator discountCalculator = new DiscountCalculator(date, gift, menuName, menuQuantity);
+        int validate = 0;
+        List<Integer> validateList = discountCalculator.getDiscountPayments();
+        validate = validateList.get(2);
+        //then
+        assertThat(validate).isEqualTo(25000);
 
     }
 
