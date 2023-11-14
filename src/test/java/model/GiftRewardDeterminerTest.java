35c39adfed3bbdae7def9729098628297e0a3b26
star-candy <el0710@naver.com>
2023-11-14 15:52:28 +0900

    feat(christmas): 증정이벤트 판단 테스트

---
+++ src/test/java/model/GiftRewardDeterminerTest.java
@@ -0,0 +1,20 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class GiftRewardDeterminerTest {
+    @Test
+    void 증정이벤트_판단_테스트() {
+        //given
+        int preDiscountPayment = 120000;
+        //when
+        GiftRewardDeterminer giftRewardDeterminer = new GiftRewardDeterminer(preDiscountPayment);
+        int validate = giftRewardDeterminer.giftRewardDetermine();
+        int giftReward = 1;
+        //then
+        assertThat(validate).isEqualTo(giftReward);
+    }
+}
